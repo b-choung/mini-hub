9dc577665bf34034bdb3c2301b5557dcad6d9538
🌿 <b.choung8@gmail.com>
2026-04-16 16:44:21 +0900

    새 앱 데이터 추가

---
+++ src/data/apps.ts
@@ -0,0 +1,34 @@
+import { App } from "@/types/apps";
+
+export const apps: App[] = [
+  {
+    id: "taskboard",
+    name: "Task Board",
+    description: "칸반 보드 스타일로 할 일 관리를 도와주는 앱입니다",
+  },
+  {
+    id: "pomodoro",
+    name: "Pomodoro Timer",
+    description: "포모도로 기법을 사용하여 집중력을 높여주는 타이머 앱입니다",
+  },
+  {
+    id: "goalTracker",
+    name: "Goal Tracker",
+    description: "목표를 설정하고 추적할 수 있는 앱입니다",
+  },
+  {
+    id: "moodTracker",
+    name: "Mood Tracker",
+    description: "기분을 기록하고 분석할 수 있는 앱입니다",
+  },
+  {
+    id: "flowerPicks",
+    name: "Flower Picks",
+    description: "원하는 꽃말에 어울리는 꽃을 추천해주는 앱입니다",
+  },
+  {
+    id: "memo",
+    name: "Memo",
+    description: "여러개의 메모를 작성하고 관리할 수 있는 앱입니다",
+  },
+];
