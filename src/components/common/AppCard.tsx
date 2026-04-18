bedf5da9882cd973367013fb50da4964d50b4ca8
🌿 <b.choung8@gmail.com>
2026-04-18 16:22:00 +0900

    app card text size 수정

--- src/components/common/AppCard.tsx
+++ src/components/common/AppCard.tsx
@@ -15,12 +15,14 @@ const AppCard = ({ app }: AppCardProps) => {
     <Link href={`/${app.id}`} className="inline-block">
       <div className="px-8 py-10 rounded-2xl shadow-md hover:shadow-xl hover:-translate-y-1 transition-all duration-200 cursor-pointer w-64 flex flex-col text-center items-center justify-center gap-4 bg-white">
         {IconComponent && (
-          <div className={`${app.color} rounded-full p-4 text-4xl flex items-center justify-center`}>
+          <div
+            className={`${app.color} rounded-full p-4 text-4xl flex items-center justify-center`}
+          >
             <IconComponent />
           </div>
         )}
         <h2 className="text-xl font-semibold line-clamp-2">{app.name}</h2>
-        <p className="text-sm text-gray-500 line-clamp-2 break-keep">
+        <p className="text-xs text-gray-500 line-clamp-2 break-keep">
           {app.description}
         </p>
       </div>
