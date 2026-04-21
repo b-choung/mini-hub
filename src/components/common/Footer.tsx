72dd25256545ce240ff547815df68e14054113c3
🌿 <b.choung8@gmail.com>
2026-04-21 15:56:31 +0900

    footer 고정으로 변경

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="p-4 bg-gray-100 text-center">
+    <footer className="sticky bottom-0 z-50 p-4 bg-gray-100 text-center">
       <p>© 2026 Mini Hub</p>
     </footer>
   );
